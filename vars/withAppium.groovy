d50cc044c38923c944f9f49f905dd8b6949c9244
simonbility <simon.anreiter@i-mobility.at>
2019-06-11 14:39:41 +0200

    add appium helper

---
+++ vars/withAppium.groovy
@@ -0,0 +1,17 @@
+
+def call(String script) {
+	dir('appium-ui-test') {
+	    deleteDir()
+	    git(
+	        url: 'https://github.com/i-mobility/appium-ui-test.git', 
+	        branch: 'master',
+	        credentialsId: '34b0992d-d6b3-4af6-9bfe-004a6987d993'
+	    )
+	}
+
+	sh """#!/bin/bash -l
+
+	./scripts/wrappium.sh ${script}
+	"""
+
+}
